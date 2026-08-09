54bdc04ec69b0ea6b0d2b4765a4af6579b8ff26b
rodatboat <ronaldo3055@gmail.com>
2026-08-09 02:35:24 -0500

    progress

--- flight_sniper.py
+++ flight_sniper.py
@@ -1,23 +1,14 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
+from playwright.sync_api import sync_playwright
+import time
 
-options = Options()
-options.add_argument("--headless")
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
-options.add_argument("--disable-gpu")
-
-driver = webdriver.Chrome(options=options)
-
-try:
-    driver.get("https://your-site.com")
+with sync_playwright() as p:
+    browser = p.chromium.launch()
+    page = browser.new_page()
     
-    # driver.find_element("id", "username").send_keys("user")
-    # driver.find_element("id", "password").send_keys("password")
-    # driver.find_element("css selector", "button[type=submit]").click()
-
-    print(driver.title)
-    print(driver.page_source)
-
-finally:
-    driver.quit()
+    try:
+        page.goto("https://www.skyscanner.com/transport/flights/dfwa/wasa/260903/260908/?adultsv2=1&childrenv2=&cabinclass=economy&rtn=1&preferdirects=true&outboundaltsenabled=false&inboundaltsenabled=false&duration=360&layover-airports=%2195673800%2C%2195673643%2C%2195673980%2C%2195673672%2C%2195673391%2C%2195673392%2C%2195673906%2C%2195673782%2C%2195673705%2C%2195673555%2C%21104120241%2C%2195673412%2C%2195673411%2C%2195674097%2C%2195673608%2C%2195673788%2C%2195673969%2C%2195673651%2C%2195673821%2C%2195673838%2C%21128667334%2C%2195673724%2C%2195673750%2C%2195565058%2C%2195565057%2C%2195565059%2C%2195673722%2C%2195674009%2C%2195673879%2C%2195673618%2C%2195673938%2C%2195674062%2C%2195673494%2C%2195673876%2C%2195673870&layover-duration=0-0")
+        time.sleep(3)
+        print(page.title())
+        print(page.content())
+    finally:
+        browser.close()
